244bed70d033e96585ad8f7d26245519dd2bced7
DannyBoyNYC <daniel.deverell@gmail.com>
2018-06-28 13:58:33 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,94 @@
-let droneId = 5;
+// 1: CLASSES
+class Recipe {
+    
+}
 
-console.log(droneId);
+let recipe = new Recipe();
+
+console.log(recipe instanceof Recipe);
+
+
+// 2: INSTANCES
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+// }
+
+// let recipe = new Recipe('123', 'Lasagna');
+
+// console.log('recipe: ' + recipe['id'] + ' ' + recipe['name']);
+
+// 3: STATIC PROPERTIES
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+// }
+// Recipe.maxHeight = 2000;
+
+// let recipe = new Recipe('123', 'Lasagna');
+// let recipe2 = new Recipe('456', 'Toast');
+
+// console.log(recipe.maxHeight);
+
+// 4: METHODS
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+  
+//   cook() {
+//       console.log('Recipe ' + this.id + ' is cookin');
+//   }
+// }
+
+// let recipe = new Recipe('123', 'Lasagna');
+// let recipe2 = new Recipe('456', 'Toast');
+
+// recipe.cook();
+// recipe2.cook();
+
+// 5: STATIC METHODS
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+  
+//   static getCompany() {
+//       console.log('hellooo');
+//   }
+  
+//   cook() {
+//       console.log('recipe ' + this.id +' is cooking');
+//   }
+// }
+
+// let recipe = new Recipe('123', 'Lasagna');
+// let recipe2 = new Recipe('456', 'Toast');
+
+// Recipe.getCompany();
+
+// 6: GETTERS AND SETTERS
+// class Recipe {
+//   constructor(id) {
+//       this._id = id;
+//   }
+
+//   get id() {
+//       console.log('in id getter');
+//       return this._id + ' TEMPORARY';
+//   }
+  
+//   set id(value) {
+//       this._id = value;
+//   }
+// }
+
+// let recipe = new Recipe('Toast');
+// recipe.id = '456';
+// console.log('recipe id: ' + recipe.id);
--- static/js/bundle.js
+++ static/js/bundle.js
@@ -96,9 +96,101 @@
 "use strict";
 
 
-var droneId = 5;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-console.log(droneId);
+// 1: CLASSES
+var Recipe = function Recipe() {
+  _classCallCheck(this, Recipe);
+};
+
+var recipe = new Recipe();
+
+console.log(recipe instanceof Recipe);
+
+// 2: INSTANCES
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+// }
+
+// let recipe = new Recipe('123', 'Lasagna');
+
+// console.log('recipe: ' + recipe['id'] + ' ' + recipe['name']);
+
+// 3: STATIC PROPERTIES
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+// }
+// Recipe.maxHeight = 2000;
+
+// let recipe = new Recipe('123', 'Lasagna');
+// let recipe2 = new Recipe('456', 'Toast');
+
+// console.log(recipe.maxHeight);
+
+// 4: METHODS
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+
+//   cook() {
+//       console.log('Recipe ' + this.id + ' is cookin');
+//   }
+// }
+
+// let recipe = new Recipe('123', 'Lasagna');
+// let recipe2 = new Recipe('456', 'Toast');
+
+// recipe.cook();
+// recipe2.cook();
+
+// 5: STATIC METHODS
+// class Recipe {
+//   constructor(id, name) {
+//       this.id = id;
+//       this.name = name;
+//   }
+
+//   static getCompany() {
+//       console.log('hellooo');
+//   }
+
+//   cook() {
+//       console.log('recipe ' + this.id +' is cooking');
+//   }
+// }
+
+// let recipe = new Recipe('123', 'Lasagna');
+// let recipe2 = new Recipe('456', 'Toast');
+
+// Recipe.getCompany();
+
+// 6: GETTERS AND SETTERS
+// class Recipe {
+//   constructor(id) {
+//       this._id = id;
+//   }
+
+//   get id() {
+//       console.log('in id getter');
+//       return this._id + ' TEMPORARY';
+//   }
+
+//   set id(value) {
+//       this._id = value;
+//   }
+// }
+
+// let recipe = new Recipe('Toast');
+// recipe.id = '456';
+// console.log('recipe id: ' + recipe.id);
 
 /***/ })
 
