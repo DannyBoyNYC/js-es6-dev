8a98a326b37b0e447b0cd4d5054a14e30bb9b54c
DannyBoyNYC <daniel.deverell@gmail.com>
2018-07-01 19:53:51 -0400

    done

--- src/services/fleet-data-service.js
+++ src/services/fleet-data-service.js
@@ -3,21 +3,21 @@
 
 export class FleetDataService {
 
-    constructor() {
-        this.cars = [];
-        this.drones = [];        
-    }    
-    
-    loadData(fleet) {
-        for (let data of fleet) {
-            switch(data.type) {
-                case 'car':
-                    this.cars.push(data);
-                    break;
-                case 'drone':
-                    this.drones.push(data);
-                    break;
-            }
+  constructor() {
+    this.cars = [];
+    this.drones = [];  
+  }
+  
+  loadData(fleet) {
+    for (let data of fleet) {
+        switch(data.type) {
+          case 'car':
+            this.cars.push(data);
+            break;
+          case 'drone':
+            this.drones.push(data);
+            break;
         }
     }
+  }
 }
--- static/js/bundle.js
+++ static/js/bundle.js
@@ -188,7 +188,7 @@ console.log(dataService.drones);
 
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -199,51 +199,51 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 // import {Drone} from '../classes/drone.js';
 
 var FleetDataService = exports.FleetDataService = function () {
-    function FleetDataService() {
-        _classCallCheck(this, FleetDataService);
+  function FleetDataService() {
+    _classCallCheck(this, FleetDataService);
 
-        this.cars = [];
-        this.drones = [];
-    }
+    this.cars = [];
+    this.drones = [];
+  }
 
-    _createClass(FleetDataService, [{
-        key: 'loadData',
-        value: function loadData(fleet) {
-            var _iteratorNormalCompletion = true;
-            var _didIteratorError = false;
-            var _iteratorError = undefined;
+  _createClass(FleetDataService, [{
+    key: 'loadData',
+    value: function loadData(fleet) {
+      var _iteratorNormalCompletion = true;
+      var _didIteratorError = false;
+      var _iteratorError = undefined;
 
-            try {
-                for (var _iterator = fleet[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                    var data = _step.value;
+      try {
+        for (var _iterator = fleet[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+          var data = _step.value;
 
-                    switch (data.type) {
-                        case 'car':
-                            this.cars.push(data);
-                            break;
-                        case 'drone':
-                            this.drones.push(data);
-                            break;
-                    }
-                }
-            } catch (err) {
-                _didIteratorError = true;
-                _iteratorError = err;
-            } finally {
-                try {
-                    if (!_iteratorNormalCompletion && _iterator.return) {
-                        _iterator.return();
-                    }
-                } finally {
-                    if (_didIteratorError) {
-                        throw _iteratorError;
-                    }
-                }
-            }
+          switch (data.type) {
+            case 'car':
+              this.cars.push(data);
+              break;
+            case 'drone':
+              this.drones.push(data);
+              break;
+          }
         }
-    }]);
+      } catch (err) {
+        _didIteratorError = true;
+        _iteratorError = err;
+      } finally {
+        try {
+          if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+          }
+        } finally {
+          if (_didIteratorError) {
+            throw _iteratorError;
+          }
+        }
+      }
+    }
+  }]);
 
-    return FleetDataService;
+  return FleetDataService;
 }();
 
 /***/ })
