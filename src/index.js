334108167561497b1ef79e1d0771a7e699577cfa
DannyBoyNYC <daniel.deverell@gmail.com>
2018-07-02 22:40:55 -0400

    done

--- src/index.js
+++ src/index.js
@@ -14,8 +14,36 @@ dataService.loadData(fleet);
 //   console.log(car.license);
 // }
 
-// let CarsForDisplay = dataService.cars;
-// console.log(CarsForDisplay.length)
-// console.log(`Sample data ${CarsForDisplay[0].model}`)
+// console.log(dataService.cars.length)
+// console.log(`${dataService.cars[0].model}`)
+
+// document.querySelector('.main').innerHTML = `${dataService.cars[0].model}`
+
+
+
+// getCarByLicense(license) {
+//   return this.cars.find(function (car) {
+//     return car.license === license
+//   })
+// }
+
+// let car = dataService.getCarByLicense('AT9900')
+//   console.log(car)
+
+
+
+// getCarsSortedByLicense() {
+//   return this.cars.sort(function (car1, car2) {
+//     if (car1.license < car2.license)
+//       return -1
+//     if (car1.license > car2.license)
+//       return 1
+//     return 0
+//   })
+// }
+
+// let cars = dataService.getCarsSortedByLicense();
+// for (let car of cars)
+  // console.log(car.license)
 
     
